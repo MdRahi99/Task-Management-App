b598f6e3fd823401af2aac1fbc789c2546c4a876
MD Rahi <fhrstation21@gmail.com>
2023-09-07 00:58:51 +0600

    setup basic context api for share user data all over the app

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Contexts/AuthProvider';
 
 const Home = () => {
+
+    const {name} = useContext(AuthContext);
+
     return (
         <div>
-            
+            {name}
         </div>
     );
 };
